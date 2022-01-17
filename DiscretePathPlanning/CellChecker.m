7ea025a4563cfa840760f8c934d04b692f1754ad
Manuel Schirmer <manuel.schirmer@mailbox.tu-dresden.de>
2022-01-17 13:20:14 +0100

    Updated case study.

--- DiscretePathPlanning/CellChecker.m
+++ DiscretePathPlanning/CellChecker.m
@@ -1,68 +1,74 @@
 classdef CellChecker
-    %CELLCHECKER Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-        Property1
-    end
+% Check if discrete trajectory (consisting of discrete discreteCells) is safe
+% against other discrete trajectories by comparing the transition systems
     
     methods
         function obj = CellChecker()
             %CELLCHECKER Construct an instance of this class
             %   Detailed explanation goes here
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
     end
     
     methods (Static)
-        
-        function uStates = createTSfromCells(CellRows)
-
-            states = ["X"+num2str(CellRows(:,1)) "Y"+num2str(CellRows(:,2))];
-            uStates= states(:,1)+states(:,2);
+        function TS = createTSfromCells(discreteCells)
+        % Create a transition system containing the states (transition from state to next state in 
+        % the list) and the entrance and exit time to each state
+            
+            X_cell = discreteCells(:, 1);
+            Y_cell = discreteCells(:, 2);
+            t_enter = discreteCells(:, 3);
+            t_exit = discreteCells(:, 4);
+            
+            states = ["X" + num2str(X_cell), "Y" + num2str(Y_cell)];
+            uStates = states(:, 1) + states(:, 2);
+            
+            TS.states = uStates;
+            TS.entranceTime = t_enter;
+            TS.exitTime = t_exit;
         end
         
         function dG = createDigraph(states)
+        % Create a digraph from states
+           
+            % TODO: Multiple cells are occupied at the same time
             idx = length(states);
             dG = digraph(states(1:(idx-1)), states(2:idx));
         end
         
-        function clean_dG = removeTheSameStates(dG2, states1,states2)
+        function dG = mergeDigraphs(dG, dG2Addd)
+        % Merge two digraphs
+            
+            newNodes = setdiff(dG2Addd.Nodes, dG.Nodes);
+            [~, id_newEdges] = setdiff(cell2mat(dG2Addd.Edges.EndNodes), cell2mat(dG.Edges.EndNodes), 'rows');
+            newEdges = dG2Addd.Edges.EndNodes(id_newEdges, :);
+            
+            dG = addnode(dG, newNodes);
+            dG = addedge(dG, newEdges(:, 1), newEdges(:, 2));
+        end
+        
+        function isSafe = isSafeTransitions(TS1, TS2)
+        % Check whether two transition systems are safe against each other
+            
+            isSafe = true;
+            
+            [overlappingStates, id_overlapping_TS1, id_overlapping_TS2] = intersect(TS1.states, TS2.states);
+            
+            if ~isempty(overlappingStates)
+                isSafe_temporal = CellChecker.isSafeTemporalDiff(TS1, TS2, id_overlapping_TS1, id_overlapping_TS2);
+                
+                if ~isSafe_temporal
+                    isSafe = false;
+                end
+            end
+        end
 
-            states2Remove= [];
-            for i=1:length(states1)
-                for j=1:length(states2)
-                    
-                    if strcmp(states1(i),states2(j))
-                        
-                        %if checkTemporalDiff(states1(i),states2(j))
-                        states2Remove = [states2Remove; states1(i)];
-                    else
-                        
-                    end
-                    
-                end
-            end
+        function isSafe = isSafeTemporalDiff(TS1, TS2, id_TS1, id_TS2)
+        % Check whether the temporal difference between relevant equal states of  
+        % two transition systems are safe against each other
             
-            clean_dG = dG2.rmnode(states2Remove);
+            isSafe = all((TS1.entranceTime(id_TS1) > TS2.exitTime(id_TS2))) || ...% Enter after other has left ...OR... 
+                     all((TS1.exitTime(id_TS1) < TS2.exitTime(id_TS2))); % Exit before other has entered
         end
-        
-        function bool = checkTemporalDiff(state1Row,state2Row)
-            bool = true;
-            if state1Row(3)< state2Row(4)
-                if state1Row(4)>stateRow(3)
-                    bool = false;
-                end
-            end
-            
-        end
-        
-        
     end
 end
 
--- DiscretePathPlanning/DiscPlanner_Prep.m
+++ DiscretePathPlanning/DiscPlanner_Prep.m
@@ -1,67 +1,125 @@
 % Case Study - Sim time = 15.3850
 
-occupiedCells_otherVehiclesFront = [
+%% Discrete trajectories other vehicles
+occupiedCells_otherVehicle1 = [
    43.0000    2.0000   15.3850   15.5050;
    44.0000    2.0000   15.5050   16.0750;
    45.0000    2.0000   16.0750   16.8050;
    46.0000    2.0000   16.8050   18.1350;
-   47.0000    2.0000   18.1350   20.3850;
+   47.0000    2.0000   18.1350   20.3850];
+
+occupiedCells_otherVehicle2 = [
    49.0000    4.0000   15.3850   15.7950;
    50.0000    4.0000   15.7950   16.2450;
-   51.0000    4.0000   16.2450   16.7650;
-   52.0000    4.0000   16.7650   17.3850;
-   53.0000    4.0000   17.3850   18.2650;
-   54.0000    4.0000   18.2650   20.3850];
+   51.0000    4.0000   16.2450   16.7650];
+%    52.0000    4.0000   16.7650   17.3850; % Comment out for unreachable states
+%    53.0000    4.0000   17.3850   18.2650; 
+%    54.0000    4.0000   18.2650   20.3850];
 
+%% Discrete Trajectories ego vehicle (decisions)
 % Decision 1: 
-egoVehicle_occCells_Traj1 = [39.0000    2.0000  190.0045  195.2533;
-   40.0000    2.0000  195.2533  195.6948;
-   40.0000    3.0000  195.6948  197.1517;
-   40.0000    4.0000  197.1517  200.8821;
-   41.0000    4.0000  200.8821  205.0521;
-   42.0000    4.0000  205.0521  210.0519;
-   43.0000    4.0000  210.0519  215.0550;
-   44.0000    4.0000  215.0550  220.0182;
-   45.0000    4.0000  220.0182  225.0564;
-   46.0000    4.0000  225.0564  230.1500;
-   47.0000    4.0000  230.1500  235.1113;
-   48.0000    4.0000  235.1113  240.0782;
-   49.0000    4.0000  240.0782  245.0335;
-   50.0000    4.0000  245.0335  250.1456;
-   51.0000    4.0000  250.1456  255.0330;
-   52.0000    4.0000  255.0330  260.0555;
-   53.0000    4.0000  260.0555  264.2109];
+egoVehicle_occCells_Traj1 = [
+   39.0000    2.0000   15.3850   15.8850;
+   40.0000    2.0000   15.8850   15.9250;
+   40.0000    3.0000   15.9250   16.0550;
+   40.0000    4.0000   16.0550   16.3750;
+   41.0000    4.0000   16.3750   16.7750;
+   42.0000    4.0000   16.7750   17.1550;
+   43.0000    4.0000   17.1550   17.5150;
+   44.0000    4.0000   17.5150   17.8550;
+   45.0000    4.0000   17.8550   18.1850;
+   46.0000    4.0000   18.1850   18.5050;
+   47.0000    4.0000   18.5050   18.8050;
+   48.0000    4.0000   18.8050   19.0950;
+   49.0000    4.0000   19.0950   19.3750;
+   50.0000    4.0000   19.3750   19.6550;
+   51.0000    4.0000   19.6550   19.9150;
+   52.0000    4.0000   19.9150   20.1750;
+   53.0000    4.0000   20.1750   20.3850];
 
 % Decision 2:
-egoVehicle_occCells_Traj2 = [39.0000    2.0000  190.0045  195.1434;
-   40.0000    2.0000  195.1434  197.7211;
-   40.0000    3.0000  197.7211  199.6944;
-   40.0000    4.0000  199.6944  200.5237;
-   41.0000    4.0000  200.5237  205.5780;
-   42.0000    4.0000  205.5780  210.0042;
-   43.0000    4.0000  210.0042  215.1352;
-   44.0000    4.0000  215.1352  220.0915;
-   45.0000    4.0000  220.0915  225.1232;
-   46.0000    4.0000  225.1232  230.0483;
-   47.0000    4.0000  230.0483  235.1656;
-   48.0000    4.0000  235.1656  240.1268;
-   49.0000    4.0000  240.1268  245.0765;
-   50.0000    4.0000  245.0765  250.1830;
-   51.0000    4.0000  250.1830  255.0651;
-   52.0000    4.0000  255.0651  260.0825;
-   53.0000    4.0000  260.0825  264.4335];
+egoVehicle_occCells_Traj2 = [
+   39.0000    2.0000   15.3850   15.8650;
+   40.0000    2.0000   15.8650   16.3050;
+   41.0000    2.0000   16.3050   16.7150;
+   42.0000    2.0000   16.7150   17.1050;
+   43.0000    2.0000   17.1050   17.4550;
+   44.0000    2.0000   17.4550   17.4650;
+   45.0000    2.0000   17.4650   17.8150;
+   46.0000    2.0000   17.8150   17.9250;
+   47.0000    2.0000   17.9250   18.1450;
+   48.0000    2.0000   18.1450   18.4650;
+   49.0000    2.0000   18.4650   18.7650;
+   50.0000    2.0000   18.7650   19.0650;
+   51.0000    2.0000   19.0650   19.3450;
+   52.0000    2.0000   19.3450   19.6250;
+   53.0000    2.0000   19.6250   19.8850;
+   54.0000    2.0000   19.8850   20.1450;
+   55.0000    2.0000   20.1450   20.3850];
 
+% Decision 3:
+egoVehicle_occCells_Traj3 = [
+   39.0000    2.0000   15.3850   15.8650;
+   40.0000    2.0000   17.4650   17.8150;
+   41.0000    2.0000   19.0650   19.3450;
+   42.0000    2.0000   20.1450   20.3850];
 
-states_Others = CellChecker.createTSfromCells(occupiedCells_otherVehiclesFront);
+% Decision 4:
+egoVehicle_occCells_Traj4 = [
+   39.0000    2.0000   15.3850   15.8650;
+   40.0000    2.0000   17.4650   17.8150;
+   40.0000    1.0000   20.1450   20.3850];
 
-dG1 = CellChecker.createDigraph(states_Others);
+%% Structure 
+decisions = cell(1, 3);
+decisions{1} = egoVehicle_occCells_Traj1;
+decisions{2} = egoVehicle_occCells_Traj2;
+decisions{3} = egoVehicle_occCells_Traj3;
+decisions{4} = egoVehicle_occCells_Traj4;
 
-states_Ego = CellChecker.createTSfromCells(egoVehicle_occCells_Traj1);
+TS_Others = cell(1, 2);
+TS_Others{1} = CellChecker.createTSfromCells(occupiedCells_otherVehicle1);
+TS_Others{2} = CellChecker.createTSfromCells(occupiedCells_otherVehicle2);
 
-dG2 = CellChecker.createDigraph(states_Ego);
+%% Compute safe decisions
+dG_safeDecisions = []; % Digraph of all safe decisions
+X_max = -inf; % Furthest reachable longitudinal cell position
+bestDecision = []; % Store best decision according to furthest reachable longitudinal cell position
+bestTransitions = []; % Transitions to furthest destination
 
-dG3 = CellChecker.removeTheSameStates(dG2,states_Ego,states_Others);
+% Check trajectrory from each decision against all trajectories from other vehicles
+for id_decision = 1:length(decisions)
+    occupied_cells_Ego = decisions{id_decision};
+    
+    TS_Ego = CellChecker.createTSfromCells(occupied_cells_Ego);
+    
+    for id_other = 1:length(TS_Others)
+        TS_Other = TS_Others{id_other};
+        isSafeTS = CellChecker.isSafeTransitions(TS_Ego, TS_Other);
+        if ~isSafeTS
+            break
+        end
+    end
+    
+    if isSafeTS
+        dG_decision = CellChecker.createDigraph(TS_Ego.states);
+        if isempty(dG_safeDecisions)
+            dG_safeDecisions = dG_decision;
+        else
+            dG_safeDecisions = CellChecker.mergeDigraphs(dG_safeDecisions, dG_decision);
+        end
+        
+        % Get safe decision which goes furthest
+        X_reachable = occupied_cells_Ego(end, 1);
+        if X_reachable > X_max
+            X_max = X_reachable;
+            bestDecision = id_decision;
+            bestTransitions = dG_decision.Edges.EndNodes;
+        end
+    end
+end
 
-plot(dG3);
-
-
+%% Show results
+plot_digraph = plot(dG_safeDecisions);
+disp(['Best decision: ', num2str(bestDecision)]);
+highlight(plot_digraph, bestTransitions(:, 1), bestTransitions(:, 2));
